e3715378e1e943f3a08662b181f0d7817c0b0324
SkyBuilder1717 <skybuilderoffical@yandex.ru>
2025-06-09 13:59:37 +0300

    rework

--- api.lua
+++ api.lua
@@ -1,24 +1,30 @@
-more_boat = {
-	LAVA = {}
-}
 local S = core.get_translator("boats")
-local function is_lava(pos)
+
+local function is_group(pos, group)
 	local nn = core.get_node(pos).name
-	return core.get_item_group(nn, "lava") ~= 0
+	return core.get_item_group(nn, group) ~= 0
 end
-local function is_water(pos)
-	local nn = core.get_node(pos).name
-	return core.get_item_group(nn, "water") ~= 0
+
+function more_boats.groups(pos, groups)
+	for _, group in pairs(groups) do
+		if is_group(pos, group) then
+			return true
+		end
+	end
+	return false
 end
+
 local function get_velocity(v, yaw, y)
 	local x = -math.sin(yaw) * v
 	local z =  math.cos(yaw) * v
 	return {x = x, y = y, z = z}
 end
+
 local function get_v(v)
 	return math.sqrt(v.x ^ 2 + v.z ^ 2)
 end
-function more_boat.on_rightclick(self, clicker)
+
+function more_boats.on_rightclick(self, clicker)
 	if not clicker or not clicker:is_player() then
 		return
 	end
@@ -45,7 +51,8 @@ function more_boat.on_rightclick(self, clicker)
 		clicker:set_look_horizontal(self.object:get_yaw())
 	end
 end
-function more_boat.on_detach_child(self, child)
+
+function more_boats.on_detach_child(self, child)
 	if child and child:get_player_name() == self.driver then
 		player_api.player_attached[child:get_player_name()] = false
 
@@ -53,17 +60,20 @@ function more_boat.on_detach_child(self, child)
 		self.auto = false
 	end
 end
-function more_boat.on_activate(self, staticdata, dtime_s)
+
+function more_boats.on_activate(self, staticdata, dtime_s)
 	self.object:set_armor_groups({immortal = 1})
 	if staticdata then
 		self.v = tonumber(staticdata)
 	end
 	self.last_v = self.v
 end
-function more_boat.get_staticdata(self)
+
+function more_boats.get_staticdata(self)
 	return tostring(self.v)
 end
-function more_boat.on_punch(self, puncher)
+
+function more_boats.on_punch(self, puncher)
 	if not puncher or not puncher:is_player() or self.removed then
 		return
 	end
@@ -90,7 +100,8 @@ function more_boat.on_punch(self, puncher)
 		end)
 	end
 end
-function more_boat.on_step(self, dtime)
+
+function more_boats.on_step(self, dtime)
 	self.v = get_v(self.object:get_velocity()) * math.sign(self.v)
 	if self.driver then
 		local driver_objref = core.get_player_by_name(self.driver)
@@ -137,11 +148,13 @@ function more_boat.on_step(self, dtime)
 	else
 		self.v = self.v - drag
 	end
+
 	local p = self.object:get_pos()
 	p.y = p.y - 0.5
 	local new_velo
 	local new_acce = {x = 0, y = 0, z = 0}
-	if not is_water(p) then
+	local group = self.node_group
+	if not more_boats.groups(p, group) then
 		local nodedef = core.registered_nodes[core.get_node(p).name]
 		if (not nodedef) or nodedef.walkable then
 			self.v = 0
@@ -154,99 +167,7 @@ function more_boat.on_step(self, dtime)
 		self.object:set_pos(self.object:get_pos())
 	else
 		p.y = p.y + 1
-		if is_water(p) then
-			local y = self.object:get_velocity().y
-			if y >= 5 then
-				y = 5
-			elseif y < 0 then
-				new_acce = {x = 0, y = 20, z = 0}
-			else
-				new_acce = {x = 0, y = 5, z = 0}
-			end
-			new_velo = get_velocity(self.v, self.object:get_yaw(), y)
-			self.object:set_pos(self.object:get_pos())
-		else
-			new_acce = {x = 0, y = 0, z = 0}
-			if math.abs(self.object:get_velocity().y) < 1 then
-				local pos = self.object:get_pos()
-				pos.y = math.floor(pos.y) + 0.5
-				self.object:set_pos(pos)
-				new_velo = get_velocity(self.v, self.object:get_yaw(), 0)
-			else
-				new_velo = get_velocity(self.v, self.object:get_yaw(),
-					self.object:get_velocity().y)
-				self.object:set_pos(self.object:get_pos())
-			end
-		end
-	end
-	self.object:set_velocity(new_velo)
-	self.object:set_acceleration(new_acce)
-end
-function more_boat.LAVA.on_step(self, dtime)
-	self.v = get_v(self.object:get_velocity()) * math.sign(self.v)
-	if self.driver then
-		local driver_objref = core.get_player_by_name(self.driver)
-		if driver_objref then
-			local ctrl = driver_objref:get_player_control()
-			if ctrl.up and ctrl.down then
-				if not self.auto then
-					self.auto = true
-					core.chat_send_player(self.driver, S("Boat cruise mode on"))
-				end
-			elseif ctrl.down then
-				self.v = self.v - dtime * 2.0
-				if self.auto then
-					self.auto = false
-					core.chat_send_player(self.driver, S("Boat cruise mode off"))
-				end
-			elseif ctrl.up or self.auto then
-				self.v = self.v + dtime * 2.0
-			end
-			if ctrl.left then
-				if self.v < -0.001 then
-					self.object:set_yaw(self.object:get_yaw() - dtime * 0.9)
-				else
-					self.object:set_yaw(self.object:get_yaw() + dtime * 0.9)
-				end
-			elseif ctrl.right then
-				if self.v < -0.001 then
-					self.object:set_yaw(self.object:get_yaw() + dtime * 0.9)
-				else
-					self.object:set_yaw(self.object:get_yaw() - dtime * 0.9)
-				end
-			end
-		end
-	end
-	local velo = self.object:get_velocity()
-	if not self.driver and
-			self.v == 0 and velo.x == 0 and velo.y == 0 and velo.z == 0 then
-		self.object:set_pos(self.object:get_pos())
-		return
-	end
-	local drag = dtime * math.sign(self.v) * (0.01 + 0.0796 * self.v * self.v)
-	if math.abs(self.v) <= math.abs(drag) then
-		self.v = 0
-	else
-		self.v = self.v - drag
-	end
-	local p = self.object:get_pos()
-	p.y = p.y - 0.5
-	local new_velo
-	local new_acce = {x = 0, y = 0, z = 0}
-	if not is_lava(p) then
-		local nodedef = core.registered_nodes[core.get_node(p).name]
-		if (not nodedef) or nodedef.walkable then
-			self.v = 0
-			new_acce = {x = 0, y = 1, z = 0}
-		else
-			new_acce = {x = 0, y = -9.8, z = 0}
-		end
-		new_velo = get_velocity(self.v, self.object:get_yaw(),
-			self.object:get_velocity().y)
-		self.object:set_pos(self.object:get_pos())
-	else
-		p.y = p.y + 1
-		if is_lava(p) then
+		if more_boats.groups(p, group) then
 			local y = self.object:get_velocity().y
 			if y >= 5 then
 				y = 5
--- init.lua
+++ init.lua
@@ -1,119 +1,101 @@
+local modname = core.get_current_modname()
+local S = core.get_translator(modname)
 
-more_boats = {}
-more_boats.boats = {
-	{
-		"Aspen Boat",
-		"more_boats:aspen_boat",
-		"more_boats_aspen_inv.png",
-		"more_boats_aspen_wield.png",
-		{"default_aspen_wood.png"},
-		"default:aspen_wood"
-	},
-	{
-		"Acacia Boat",
-		"more_boats:acacia_boat",
-		"more_boats_acacia_inv.png",
-		"more_boats_acacia_wield.png",
-		{"default_acacia_wood.png"},
-		"default:acacia_wood"
-	},
-	{
-		"Pine Boat",
-		"more_boats:pine_boat",
-		"more_boats_pine_inv.png",
-		"more_boats_pine_wield.png",
-		{"default_pine_wood.png"},
-		"default:pine_wood"
-	},
-	{
-		"Jungle Wood Boat",
-		"more_boats:jungle_boat",
-		"more_boats_jungle_inv.png",
-		"more_boats_jungle_wield.png",
-		{"default_junglewood.png"},
-		"default:junglewood"
-	},
-	{
-		"Obsidian Boat",
-		"more_boats:obsidian_boat",
-		"more_boats_obsidian_inv.png",
-		"more_boats_obsidian_wield.png",
-		{"default_obsidian.png"},
-		"default:obsidian",
-		true
+more_boats = {
+	registered_boats = {},
+	boats = {
+		["more_boats:aspen_boat"] = {
+			description = S("Aspen Boat"),
+			texture = "default_aspen_wood.png",
+			material = "default:aspen_wood"
+		},
+		["more_boats:acacia_boat"] = {
+			description = S("Acacia Boat"),
+			texture = "default_acacia_wood.png",
+			material = "default:acacia_wood"
+		},
+		["more_boats:pine_boat"] = {
+			description = S("Pine Boat"),
+			texture = "default_pine_wood.png",
+			material = "default:pine_wood"
+		},
+		["more_boats:jungle_boat"] = {
+			description = S("Jungle Wood Boat"),
+			texture = "default_junglewood.png",
+			material = "default:junglewood"
+		},
+		["more_boats:obsidian_boat"] = {
+			description = S("Obsidian Boat"),
+			texture = "default_obsidian.png",
+			material = "default:obsidian",
+			group = {"lava"},
+			no_fuel = true
+		}
 	}
 }
-local S = minetest.get_translator("more_boats")
-local modpath = minetest.get_modpath("more_boats")
+
+local modpath = core.get_modpath(modname)
 dofile(modpath .."/api.lua")
-minetest.clear_craft({
+
+core.clear_craft({
 	output = "boats:boat",
 	recipe = {
 		{"",           "",           ""          },
 		{"group:wood", "",           "group:wood"},
 		{"group:wood", "group:wood", "group:wood"},
-	},
+	}
 }) 
-minetest.register_craft({
+core.register_craft({
 	output = "boats:boat",
 	recipe = {
-		{"", "", ""},
 		{"default:wood", "", "default:wood"},
 		{"default:wood", "default:wood", "default:wood"},
 	},
 })
-local function is_water(pos)
-	local nn = minetest.get_node(pos).name
-	return minetest.get_item_group(nn, "water") ~= 0
-end
-local function is_lava(pos)
-	local nn = minetest.get_node(pos).name
-	return minetest.get_item_group(nn, "lava") ~= 0
-end
-for i, def in ipairs(more_boats.boats) do
-	local desc = def[1]
-	local name = def[2]
-	local texture_inv = def[3]
-	local texture_wield = def[4]
-	local texture_boat = def[5]
-	local material = def[6]
-	local isnt_lava = def[7]
+
+function more_boats.register_boat(name, def)
+	more_boats.registered_boats[name] = def
+	local group = def.group
+	if not group then
+		group = {}
+	end
+	table.insert(group, "water")
+
 	local boat_def = {
 		initial_properties = {
 			physical = true,
 			collisionbox = {-0.5, -0.35, -0.5, 0.5, 0.3, 0.5},
 			visual = "mesh",
 			mesh = "boats_boat.obj",
-			textures = texture_boat,
+			textures = {def.texture},
 		},
 		driver = nil,
+		node_group = group,
 		v = 0,
 		last_v = 0,
 		removed = false,
 		auto = false
 	}
-	boat_def.on_rightclick = more_boat.on_rightclick
-	boat_def.on_detach_child = more_boat.on_detach_child
-	boat_def.on_activate = more_boat.on_step
-	boat_def.get_staticdata = more_boat.get_staticdata
-	boat_def.on_punch = more_boat.on_punch
-	if not isnt_lava then
-		boat_def.on_step = more_boat.on_step
-	else
-		boat_def.on_step = more_boat.LAVA.on_step
-	end
-	minetest.register_entity(name, boat_def)
-	minetest.register_craftitem(name, {
-		description = S(desc),
-		inventory_image = texture_inv,
-		wield_image = texture_wield,
+
+	boat_def.on_rightclick = more_boats.on_rightclick
+	boat_def.on_detach_child = more_boats.on_detach_child
+	boat_def.on_activate = more_boats.on_step
+	boat_def.get_staticdata = more_boats.get_staticdata
+	boat_def.on_punch = more_boats.on_punch
+	boat_def.on_step = more_boats.on_step
+
+	core.register_entity(name, boat_def)
+	core.register_craftitem(name, {
+		description = def.description,
+		inventory_image = def.texture.."^[mask:more_boats_inv_mask.png",
+		wield_image = def.texture.."^[mask:more_boats_wield_mask.png",
 		wield_scale = {x = 2, y = 2, z = 1},
 		liquids_pointable = true,
 		groups = {flammable = 2},
 		on_place = function(itemstack, placer, pointed_thing)
 			local under = pointed_thing.under
-			local node = minetest.get_node(under)
-			local udef = minetest.registered_nodes[node.name]
+			local node = core.get_node(under)
+			local udef = core.registered_nodes[node.name]
 			if udef and udef.on_rightclick and
 					not (placer and placer:is_player() and
 					placer:get_player_control().sneak) then
@@ -123,42 +105,51 @@ for i, def in ipairs(more_boats.boats) do
 			if pointed_thing.type ~= "node" then
 				return itemstack
 			end
-			if not isnt_lava then
-				if not is_water(pointed_thing.under) then
-					return itemstack
-				end
-			else
-				if not is_lava(pointed_thing.under) then
-					return itemstack
-				end
+			if not more_boats.groups(pointed_thing.under, group) then
+				return itemstack
 			end
 			pointed_thing.under.y = pointed_thing.under.y + 0.5
-			boat = minetest.add_entity(pointed_thing.under, name)
+			boat = core.add_entity(pointed_thing.under, name)
 			if boat then
 				if placer then
 					boat:set_yaw(placer:get_look_horizontal())
 				end
 				local player_name = placer and placer:get_player_name() or ""
-				if not minetest.is_creative_enabled(player_name) then
+				if not core.is_creative_enabled(player_name) then
 					itemstack:take_item()
 				end
 			end
 			return itemstack
 		end,
 	})
-	minetest.register_craft({
+
+	local m = def.material
+	core.register_craft({
 		output = name,
 		recipe = {
-			{"", "", ""},
-			{material, "", material},
-			{material, material, material},
+			{m, "",m},
+			{m, m, m},
 		}
 	})
-	if not is_lava then
-		minetest.register_craft({
-			type = "fuel",
-			recipe = name,
-			burntime = 20,
-		})
+
+	if not def.no_fuel then
+		if def.cooking then
+			core.register_craft({
+				type = "cooking",
+				output = def.cooking,
+				recipe = name,
+				cooktime = 15,
+			})
+		else
+			core.register_craft({
+				type = "fuel",
+				recipe = name,
+				burntime = 20,
+			})
+		end
 	end
+end
+
+for name, def in pairs(more_boats.boats) do
+	more_boats.register_boat(name, def)
 end
